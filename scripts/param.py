739cb25943b1f63aac508c8830ebbefd463b71da
Thibault ROBERT <tbt.rbt@gmail.com>
2020-10-03 22:08:01 +0200

    sntx

--- scripts/param.py
+++ scripts/param.py
@@ -95,9 +95,9 @@ class Param:
            self.axis0.motor.config.pre_calibrated is True:
            """ and self.axis1.encoder.config.pre_calibrated is True and \
            self.axis1.motor.config.pre_calibrated is True"""
-           # flag = "INIT"
+           # flag = 'INIT'
            flag = input("ATTENTION ! : RAZ des moteurs, maintenir le robot en l'air: [ENTRER pour continuer]")
-           if flag is "" :
+           if flag == '' :
                 time.sleep()
                 self.axis0.encoder.config.use_index = True
                 self.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
