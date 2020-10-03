7a14881c4b5f0166c1d9a39556f4900d44e8a0b6
Thibault ROBERT <tbt.rbt@gmail.com>
2020-10-03 19:06:49 +0200

    odrv = self.odrv0

--- scripts/param.py
+++ scripts/param.py
@@ -18,104 +18,104 @@ class Param:
 
     def config(self):
         # 40Amp max dans le moteur (gros couple et sécurité pour pas fumer le moteur)
-        self.odrv0.axis0.motor.config.current_lim = 10
-        self.odrv0.axis1.motor.config.current_lim = 10
+        odrv.axis0.motor.config.current_lim = 10
+        odrv.axis1.motor.config.current_lim = 10
 
         # vmax en tick/s les encodeurs font 8192 tick/tours
         # controller.*.vel_limite prend le pas sur trap_traj.*.vel_limt
-        self.odrv0.axis0.controller.config.vel_limit = 50000
-        self.odrv0.axis1.controller.config.vel_limit = 50000
+        odrv.axis0.controller.config.vel_limit = 50000
+        odrv.axis1.controller.config.vel_limit = 50000
 
         # trap_traj parametrage des valeurs limit du comportement dynamique
-        self.odrv0.axis1.trap_traj.config.vel_limit = 10000
-        self.odrv0.axis0.trap_traj.config.vel_limit = 10000
+        odrv.axis1.trap_traj.config.vel_limit = 10000
+        odrv.axis0.trap_traj.config.vel_limit = 10000
 
-        self.odrv0.axis0.trap_traj.config.accel_limit = 7000
-        self.odrv0.axis1.trap_traj.config.accel_limit = 7000
+        odrv.axis0.trap_traj.config.accel_limit = 7000
+        odrv.axis1.trap_traj.config.accel_limit = 7000
 
-        self.odrv0.axis0.trap_traj.config.decel_limit = 7000
-        self.odrv0.axis1.trap_traj.config.decel_limit = 7000
+        odrv.axis0.trap_traj.config.decel_limit = 7000
+        odrv.axis1.trap_traj.config.decel_limit = 7000
 
         # test avec  calib_saved.py
-        # self.odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-        # self.odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        # odrv.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        # odrv.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
     def raz(self):
         # Fonction de remise à zero des moteurs pour initialisation si calib déja faite
         flag = 'N'
         flag = input("Le robot est hors sol ? (Y or N)")
-        self.odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-        self.odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        odrv.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        odrv.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
         if flag == 'Y':
-            """self.odrv0.axis0.controller.config.vel_limit = 7000
-            self.odrv0.axis1.controller.config.vel_limit = 7000
+            """odrv.axis0.controller.config.vel_limit = 7000
+            odrv.axis1.controller.config.vel_limit = 7000
 
             # trap_traj parametrage des valeurs limit du comportement dynamique
-            self.odrv0.axis1.trap_traj.config.vel_limit = 1000
-            self.odrv0.axis0.trap_traj.config.vel_limit = 1000
+            odrv.axis1.trap_traj.config.vel_limit = 1000
+            odrv.axis0.trap_traj.config.vel_limit = 1000
 
-            self.odrv0.axis0.trap_traj.config.accel_limit = 750
-            self.odrv0.axis1.trap_traj.config.accel_limit = 750
+            odrv.axis0.trap_traj.config.accel_limit = 750
+            odrv.axis1.trap_traj.config.accel_limit = 750
 
-            self.odrv0.axis0.trap_traj.config.decel_limit = 750
-            self.odrv0.axis1.trap_traj.config.decel_limit = 750"""
+            odrv.axis0.trap_traj.config.decel_limit = 750
+            odrv.axis1.trap_traj.config.decel_limit = 750"""
             # Remise en position 0 des moteurs pour initialisation
-            self.odrv0.axis0.controller.move_to_pos(0)
-            self.odrv0.axis1.controller.move_to_pos(0)
+            odrv.axis0.controller.move_to_pos(0)
+            odrv.axis1.controller.move_to_pos(0)
             print("Poser le robot au sol")
             time.sleep(10)
 
     def calib(self):
         # Fonction de calibration sans condition
         print("starting calibration...")
-        self.odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-        self.odrv0.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+        odrv.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+        odrv.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 
-	    #"" fonction sauvegarde calibration""
-        while self.odrv0.axis0.current_state != 1 and self.odrv0.axis1.current_state != 1:
+        #"" fonction sauvegarde calibration""
+        while odrv.axis0.current_state != 1 and odrv.axis1.current_state != 1:
             time.sleep(0.1)
 
         # Met les moteurs en boucle fermée
-        self.odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-        self.odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        odrv.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        odrv.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
         # flag calib Done !
         odrv0.axis0.motor.config.pre_calibrated = True
         odrv0.axis1.motor.config.pre_calibrated = True
 
-        #permet d'attendre que le tunning soit "réglé", sinon robot dérive
+        # permet d'attendre que le tunning soit "réglé", sinon robot dérive
         time.sleep(1)
 
     def calib_bis(self):
 
-        #test 1 moteur, calib/Tun si pas déjà faite et saved en config
-        if self.odrv0.axis0.encoder.config.pre_calibrated is True and \
-            self.odrv0.axis0.motor.config.pre_calibrated is True :
-            self.odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        # test 1 moteur, calib/Tun si pas déjà faite et saved en config
+        if odrv.axis0.encoder.config.pre_calibrated is True and \
+           odrv.axis0.motor.config.pre_calibrated is True :
+            odrv.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
             time.sleep(1)
         else :
             # Fonction de calibration sans condition
             print("starting calibration...")
-            self.odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+            odrv.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 
             # Met les moteurs en boucle fermée
-            self.odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+            odrv.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
             # flag calib Done !
-            self.odrv0.axis0.encoder.config.pre_calibrated = True
-            self.odrv0.axis0.motor.config.pre_calibrated = True
+            odrv.axis0.encoder.config.pre_calibrated = True
+            odrv.axis0.motor.config.pre_calibrated = True
 
 
     def save_config(self):
 
         # test pour garder la config de tunning moteurs entre deux runs.
-        while self.odrv0.axis0.current_state != 1 and self.odrv0.axis1.current_state != 1:
+        while odrv.axis0.current_state != 1 and odrv.axis1.current_state != 1:
             time.sleep(0.1)
 
-        self.odrv0.save_configuration()
+        odrv.save_configuration()
 
 
     def unlock_wheels(self):
         # AXIS_STATE_IDLE , libère le moteur : boucle ouverte
-        self.odrv0.axis0.requested_state = AXIS_STATE_IDLE
-        self.odrv0.axis1.requested_state = AXIS_STATE_IDLE
+        odrv.axis0.requested_state = AXIS_STATE_IDLE
+        odrv.axis1.requested_state = AXIS_STATE_IDLE
