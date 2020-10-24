e4e84400a2e9cc2cad6fb9bf992b1854f1c2326c
Thibault ROBERT <tbt.rbt@gmail.com>
2020-10-24 12:21:20 +0200

    ajout de temps entre chaque action car non fctionnel

--- scripts/calibration_reinit.py
+++ scripts/calibration_reinit.py
@@ -10,13 +10,12 @@ print("Recherche Odrive...")
 odrv0 = odrive.find_any()
 print("Effacement de la configuration précédente")
 odrv0.erase_configuration()
-sleep(3)
+sleep(5)
 
 print("Lancement de la calibration pour chaque moteur")
 odrv0.axis0.requested_state = AXIS_STATE_MOTOR_CALIBRATION
 odrv0.axis1.requested_state = AXIS_STATE_MOTOR_CALIBRATION
-while odrv0.axis0.requested_state is 4 and odrv0.axis1.requested_state is 4:
-    sleep(0.5)
+sleep(5)
 print("Définition de l'état pré-calibré pour chaque moteur")
 odrv0.axis0.motor.config.pre_calibrated = True
 odrv0.axis1.motor.config.pre_calibrated = True
@@ -29,8 +28,7 @@ sleep(0.5)
 print("Lancement de la recherche d'index pour chaque encodeur")
 odrv0.axis0.requested_state = AXIS_STATE_ENCODER_INDEX_SEARCH
 odrv0.axis1.requested_state = AXIS_STATE_ENCODER_INDEX_SEARCH
-while odrv0.axis0.requested_state is 7 and odrv0.axis1.requested_state is 7:
-    sleep(0.5)
+sleep(5)
 print("Définition de l'état pré-calibré pour chaque encodeur")
 odrv0.axis0.encoder.config.pre_calibrated = True
 odrv0.axis1.encoder.config.pre_calibrated = True
