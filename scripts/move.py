b2e7bf7293a72d917136a26aa224e3237b359b58
Thibault ROBERT <tbt.rbt@gmail.com>
2020-10-25 01:29:20 +0200

    close_loop_control collapse! test witch wait_end_move

--- scripts/move.py
+++ scripts/move.py
@@ -54,21 +54,21 @@ class Move:
         # Définition de la distance à parcourir en tics vis à vis de la position actuelle avec le moteur de gauche:
         target0 = - (axis0.encoder.pos_estimate + self.nbTics * distance) / self.perimetreRoue
         target1 = (axis0.encoder.pos_estimate + self.nbTics * distance) / self.perimetreRoue
-        print("pos_estimate 0: %d" % axis0.encoder.pos_estimate )
+        print("pos_estimate 0: %d" % axis0.encoder.pos_estimate)
         print("target0 : %d" % target0)
-        print("pos_estimate 1: %d" % axis1.encoder.pos_estimate )
+        print("pos_estimate 1: %d" % axis1.encoder.pos_estimate)
         print("target1 : %d" % target1)
         # Début de la translation :
         axis0.controller.move_to_pos(target0)
         axis1.controller.move_to_pos(target1)
 
         # boucle de régulation de la position
-        while axis0.encoder.pos_estimate > abs(target0-self.errorMax) or axis1.encoder.pos_estimate < abs(target1+self.errorMax):
-            sleep(0.001)
+        #while axis0.encoder.pos_estimate > abs(target0-self.errorMax) or axis1.encoder.pos_estimate < abs(target1+self.errorMax):
+        #    sleep(0.001)
 
         # fonction pour réguler la fonction move_to_pos(nb_tics_distance)
-        #self.wait_end_move(strMouv, axis0, target0, self.errorMax)
-        #self.wait_end_move(strMouv, axis1, target1, self.errorMax)
+        self.wait_end_move(strMouv, axis0, target0, self.errorMax)
+        self.wait_end_move(strMouv, axis1, target1, self.errorMax)
 
         print("Translation Terminée !")
         sleep(1)
