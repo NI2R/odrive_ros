aaa43cfbb27521d5ce357c20c76e6736bd47e15c
Thibault ROBERT <tbt.rbt@gmail.com>
2020-10-25 02:32:31 +0100

    test 1 moteur

--- scripts/tests.py
+++ scripts/tests.py
@@ -11,8 +11,8 @@ from math import pi
 
 def Config(odrv):
 
-    odrv.axis0.motor.config.current_lim = 7
-    odrv.axis1.motor.config.current_lim = 7
+    odrv.axis0.motor.config.current_lim = 20
+    odrv.axis1.motor.config.current_lim = 20
 
     # vmax en tick/s les encodeurs font 8192 tick/tours
     # controller.*.vel_limite prend le pas sur trap_traj.*.vel_limt
@@ -34,10 +34,8 @@ def Calibration(odrv):
 
 
     print("starting calibration...")
-    odrv.axis0.motor.config.pre_calibrated = False
-    odrv.axis1.motor.config.pre_calibrated = False
     odrv.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-    odrv.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+    #odrv.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 
     # Attente fin de la calib, et retour état par défaut IDLE_STATE
     while odrv.axis0.current_state != 1 and odrv.axis1.current_state != 1:
@@ -45,7 +43,7 @@ def Calibration(odrv):
 
     # Met les moteurs en boucle fermée
     odrv.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-    odrv.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+    #odrv.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
     time.sleep(3)
 
 
@@ -80,6 +78,37 @@ def Test_move_incremental(odrv, distance):
     print("j'attends 5sec avant de finir")
     time.sleep(5)
 
+def Test_move_to_pos(odrv, distance):
+
+    # définition des données :
+    nb_tics = 8192
+    diametre_roue_mm = 80
+    perimetre_roue_mm = diametre_roue_mm * pi
+    distance_tics_G = - (nb_tics * distance) / perimetre_roue_mm
+    distance_tics_D = (nb_tics * distance) / perimetre_roue_mm
+
+    print('test move_to_pos(distance_tics)  allé')
+
+    odrv.axis0.controller.move_to_pos(distance_tics_G)
+    #odrv.axis1.controller.move_to_pos(distance_tics_D)
+    print("pos_estimate 0: %d" % odrv.axis0.encoder.pos_estimate)
+    print("pos_estimate 1: %d" % odrv.axis1.encoder.pos_estimate)
+
+    #print("attend idle state")
+    #print("control_mode  gauche : ", odrv.axis0.controller.current_state)
+    #print("control_mode droite : ", odrv.axis1.controller.current_state)
+    #print("_______")
+    #while odrv.axis0.controller.control_mode != 1 and odrv.axis1.controller.conrtrol_mode != 1:
+
+    #    print("control_mode gauche : ", odrv.axis0.controller.current_state)
+    #    print("control_mode droite : ", odrv.axis1.controller.current_state)
+    #    time.sleep(0.2)
+
+    print("j'attends 5sec avant de finir")
+    time.sleep(5)
+    print("pos_estimate 0: %d" % odrv.axis0.encoder.pos_estimate)
+    print("pos_estimate 1: %d" % odrv.axis1.encoder.pos_estimate)
+
 def Test_diametre_roue(odrv):
 
     print('lancement move_incremental 1 tour de roue')
@@ -93,5 +122,6 @@ odrv = odrive.find_any()
 print('Odrive found ! ')
 Config(odrv)
 Calibration(odrv)
-Test_move_incremental(odrv,200)
+#Test_move_incremental(odrv,200)
+Test_move_to_pos(odrv,500)
 # Test_diametre_roue(odrv)
