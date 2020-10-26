c2e2bd2c4bfb5459aa1f0cc315bc8e13a4373178
Thibault ROBERT <tbt.rbt@gmail.com>
2020-10-26 22:28:28 +0100

    test rotation et carré!

--- scripts/param.py
+++ scripts/param.py
@@ -54,8 +54,7 @@ class Param:
         # Forcer la position zero des encoders
         print("Remise à zéro des moteurs")
         test = str(raw_input("Le robot est-il en l'air ? : [y] pour continuer \n"))
-        print(test)
-        if test == "y" :
+        if test == 'y' :
             self.odrv.axis0.controller.move_to_pos(0)
             self.odrv.axis1.controller.move_to_pos(0)
             sleep(1)
