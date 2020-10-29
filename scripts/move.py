f18f2ef425ee34d91561c984d3e58d171a46565f
Yann Locatelli <yann.locatelli2@gmail.com>
2020-10-29 04:56:03 +0100

    Change value of limite_detection to 600

--- scripts/move.py
+++ scripts/move.py
@@ -36,7 +36,7 @@ class Move:
         self.sharp_list = [0, 1, 2, 3, 4]  # liste des capteurs
         self.SenOn = [0 for i in range(len(self.sharp_list))]  # liste flag detection pour chaque capteur
         self.Sen_count = 0 # compteur de detection
-        self.limite_detection = 700
+        self.limite_detection = 600
 
         # Définition des distances et vitesses :
         self.distance0_mm = 0
